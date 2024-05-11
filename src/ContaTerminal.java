0f5e1d587142d68aa87572b645022303876d00ed
dwbessa <danielbessa01@gmail.com>
2024-05-11 13:53:11 -0300

    feat: final version, all works perfectly

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class ContaTerminal {
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, World!");
+		//Conhecer e importar a classe Scanner
+		//Exibir as mensagens para o nosso usuário
+		//Obter pela scanner os valores digitados no terminal
+		//Exibir a mensagem da conta criaa
+
+		int		numeroConta;
+		String	agenciaConta;
+		String	nomeUsuario;
+		int		saldoConta;
+
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("Por favor, insira o número da conta");
+		numeroConta = myScanner.nextInt();
+		myScanner.nextLine();
+		System.out.println("Agora, digite a sua agência");
+		agenciaConta = myScanner.nextLine();
+		System.out.println("Digite seu nome completo");
+		nomeUsuario = myScanner.nextLine();
+		System.out.println("Pra finalizar, digite o saldo da conta");
+		saldoConta = myScanner.nextInt();
+		myScanner.nextLine();
+		System.out.println("Olá " + nomeUsuario + ", obrigado por criar uma" +
+		" conta em nosso banco, sua agência é " + agenciaConta + ", conta " +
+		numeroConta + " e seu saldo de R$" + saldoConta + ".00 já está disponível" +
+		" para saque");
 	}
 }
